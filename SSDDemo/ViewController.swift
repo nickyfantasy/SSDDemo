2d72ee21abf6af81d586f0dd807c8dd671247fc0
Nicky <nicky@baidu.com>
2017-11-06 20:23:55 -0800

    Initial Commit

---
+++ SSDDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SSDDemo
+//
+//  Created by Nicky Chan on 11/6/17.
+//  Copyright © 2017 PaddlePaddle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
